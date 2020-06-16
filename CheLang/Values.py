8b668e33f3fecacda0ecc53459b36fe552ea62e1
frandilu <francodilu@gmail.com>
2020-06-16 16:32:50 -0300

    Argentina function bugfix (again)

--- CheLang/Values.py
+++ CheLang/Values.py
@@ -600,19 +600,22 @@ class BuiltInFunction(BaseFunction):
         print("\n¡VIVA LA PATRIA!\n\n¡VIVA!\n")
         imageUri = os.path.abspath(os.environ["CheLangPath"]) + "\\assets\\Argentina.jpg"
         soundUri = os.path.abspath(os.environ["CheLangPath"]) + "\\assets\\Malvinas.mp3"
+        notDone = True
         try:
             os.startfile(imageUri)
+            notDone = False
             ctypes.windll.user32.SystemParametersInfoW(20, 0, imageUri , 0)
             playsound(soundUri)
         except:
-            imageUri = os.path.abspath(os.getcwd()) + "\\assets\\Argentina.jpg"
-            soundUri = os.path.abspath(os.getcwd()) + "\\assets\\Malvinas.mp3"
-            try:
-                os.startfile(imageUri)
-                ctypes.windll.user32.SystemParametersInfoW(20, 0, imageUri , 0)
-                playsound(soundUri)
-            except FileNotFoundError:
-                print("Pasó algo, no pude cargar las cosas")
+            if notDone:
+                imageUri = os.path.abspath(os.getcwd()) + "\\assets\\Argentina.jpg"
+                soundUri = os.path.abspath(os.getcwd()) + "\\assets\\Malvinas.mp3"
+                try:
+                    os.startfile(imageUri)
+                    ctypes.windll.user32.SystemParametersInfoW(20, 0, imageUri , 0)
+                    playsound(soundUri)
+                except FileNotFoundError:
+                    print("Pasó algo, no pude cargar las cosas")
         finally:
             return RTResult().success(Number.null)
     execute_argentina.arg_names = []
